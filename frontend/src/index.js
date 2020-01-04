48ed4043b326620d68364e52742477bde2108c89
LoKi1237 <loki1237@yandex.ru>
2020-01-05 02:38:32 +0300

    Changed code style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducers } from './reducers/index';
 
-const store = createStore( reducers );
+const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, 
-        document.getElementById( 'root' ));
+    document.getElementById( 'root' )
+);
