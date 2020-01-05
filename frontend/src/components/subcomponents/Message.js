91bd3ec8d3452d34dc5f792872d0490d01586fd8
LoKi1237 <loki1237@yandex.ru>
2020-01-05 03:20:51 +0300

    Added close button

--- frontend/src/components/subcomponents/Message.js
+++ frontend/src/components/subcomponents/Message.js
@@ -1,42 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styles from './Message.css';
 
-class Message extends React.Component {
-    componentDidMount() {
-        let timerID = setInterval( () => {
-            if( new Date().getTime() >= this.props.message.lifeTime ) {
-                this.props.closeMessage();
-                clearInterval( timerID );
-            }
-        }, 100 );
-    }
-
-    render() {
-        return (
-            <div className={`${styles.Message} ${styles[`${this.props.message.color}`]}`}>
-                <span>{this.props.message.text}</span>
-            </div>
-        )
-    }
+function Message(props) {
+    return (
+        <div className={`${styles.Message} ${styles[`${props.data.color}`]}
+            ${props.data.closing ? styles.close : ""}`}>
+            <span>{props.data.text}</span>
+            <button className={styles["close-button"]}
+                onClick={props.closeMessage}>
+            </button>
+        </div>
+    );
 }
 
-//===============================================================
-
-function mapStateToProps( state ) {
-    return {
-        message: state.message
-    }
-}
-
-function mapDispatchToProps( dispatch ) {
-    return {
-        closeMessage: () => {
-            dispatch({
-                type: "CLOSE_MESSAGE"
-            })
-        }
-    }
-}
-
-export default connect( mapStateToProps, mapDispatchToProps )( Message );
+export default Message;
