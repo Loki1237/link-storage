873d2a3da203b1baea1697842972193c03b37d41
LoKi1237 <loki1237@yandex.ru>
2020-01-15 12:35:30 +0300

    Stylized code

--- frontend/src/reducers/modalsReducer.js
+++ frontend/src/reducers/modalsReducer.js
@@ -3,29 +3,29 @@ const initialState = {
     ChangeLink: false,
     ShowHiddenLinks: false,
     ChangePassword: false,
-    ChangePINcode: false,
+    ChangePIN: false,
     DeleteUser: false
-}
+};
 
-export default function( state = initialState, action ) {
-    switch( action.type ) {
+export default function(state = initialState, action) {
+    switch (action.type) {
         case "OPEN_ADD_LINK":
-            return Object.assign({}, initialState, { AddLink: true });
+            return { ...initialState, AddLink: true };
 
         case "OPEN_CHANGE_LINK":
-            return Object.assign({}, initialState, { ChangeLink: true });
+            return { ...initialState, ChangeLink: true };
         
         case "OPEN_SHOW_HIDDEN_LINKS":
-            return Object.assign({}, initialState, { ShowHiddenLinks: true });
+            return { ...initialState, ShowHiddenLinks: true };
 
         case "OPEN_CHANGE_PASSWORD":
-            return Object.assign({}, initialState, { ChangePassword: true });
+            return { ...initialState, ChangePassword: true };
 
         case "OPEN_CHANGE_PIN_CODE":
-            return Object.assign({}, initialState, { ChangePINcode: true });
+            return { ...initialState, ChangePIN: true };
         
         case "OPEN_DELETE_USER":
-            return Object.assign({}, initialState, { DeleteUser: true });
+            return { ...initialState, DeleteUser: true };
 
         case "CLOSE_MODAL":
             return initialState;
