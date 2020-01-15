45511fcd94f02018ab309b13ade20dd2ac723fd6
LoKi1237 <loki1237@yandex.ru>
2020-01-15 12:25:09 +0300

    Added badge

--- frontend/src/components/subcomponents/Message.js
+++ frontend/src/components/subcomponents/Message.js
@@ -5,7 +5,10 @@ function Message(props) {
     return (
         <div className={`${styles.Message} ${styles[`${props.data.color}`]}
             ${props.data.closing ? styles.close : ""}`}>
+            
+            <div className={`${styles.badge} ${styles[`badge-${props.data.color}`]}`}></div>
             <span>{props.data.text}</span>
+
             <button className={styles["close-button"]}
                 onClick={props.closeMessage}>
             </button>
