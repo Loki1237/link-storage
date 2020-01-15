a398b1a35cfa69b33d4d5f48f3f2ed7251892ab1
LoKi1237 <loki1237@yandex.ru>
2020-01-15 11:59:57 +0300

    Added method 'showMessage'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import styles from './App.css';
 import { Router, Route } from 'react-router-dom';
 import history from './history';
+import styles from './App.css';
 
 import { connect } from 'react-redux';
-import { setLanguage, authorization } from './actions/index';
+import { setAppData } from './actions/index';
+import { translate } from './language/index';
 
 import NavBar from './containers/NavBar';
 import EntryBarAut from './containers/EntryBarAut';
@@ -13,7 +14,6 @@ import MyLinks from './containers/MyLinks';
 import UserMenu from './containers/UserMenu';
 
 import LanguageSwitch from './components/subcomponents/LanguageSwitch';
-import Modal from './components/subcomponents/Modal';
 import Message from './components/subcomponents/Message';
 
 class App extends React.Component {
@@ -21,70 +21,88 @@ class App extends React.Component {
         super(props);
         this.showMessage = this.showMessage.bind(this);
         this.closeMessage = this.closeMessage.bind(this);
-        this.updateUserProps = this.updateUserProps.bind(this);
         this.exit = this.exit.bind(this);
         this.state = {
-            
-        }
+            Message: {
+                isVisible: false,
+                closing: false,
+                text: "",
+                color: "",
+                timer: 0
+            }
+        };
     }
 
     componentDidMount() {
-        let token = localStorage.getItem("token");
-        let lang = localStorage.getItem("lang");
-        if( token ) {
+        const token = localStorage.getItem("token");
+        const lang = localStorage.getItem("lang");
+
+        if (token) {
             fetch(`/api/users/token/${token}`)
-                .then( res => res.json() )
-                .then( 
-                    user => {
-                        if( !user.error ) {
-                            this.props.authorization({ data: user });
-                            history.push("/bookmarks");
-                        }
-                        if( user.error ) {
-                            history.push("/aut");
-                        }
-                    } 
-                )
-                .catch( () => history.push("/aut") );
+                .then(res => res.json())
+                .then(user => {
+                    if (!user.error) {
+                        this.props.setAppData({ user });
+                        history.push("/bookmarks");
+                    }
+                    if (user.error) {
+                        history.push("/aut");
+                    }
+                })
+                .catch(() => {
+                    history.push("/aut");
+                });
         } else {
             history.push("/aut");
         }
-        if( lang ) {
-            this.props.setLanguage({ lang });
+
+        if (lang) {
+            translate(lang);
+            this.props.setAppData({ lang });
         }
     }
 
-    showMessage(text) {
-        this.setState({ message: {
-            isVisible: true,
-            text,
-            lifeTime: new Date().getTime() + 3000
-        } })
+    showMessage(data) {
+        this.closeMessage();
+        setTimeout(() => {
+            this.setState({
+                Message: {
+                    isVisible: true,
+                    closing: false,
+                    text: data.text,
+                    color: data.color,
+                    timer: setTimeout(this.closeMessage, 5000)
+                }
+            });
+        }, 100);
     }
 
     closeMessage() {
-        this.setState({ message: {
-            isVisible: false,
-            text: "",
-            lifeTime: 0
-        } })
-    }
-
-    updateUserProps() {
-        let token = localStorage.getItem('token');
-        fetch(`/api/users/token/${token}`)
-            .then( res => res.json() )
-            .then( user => {
-                this.props.authorization({ data: user });
-            })
+        clearTimeout(this.state.Message.timer);
+        this.setState({ 
+            Message: { 
+                ...this.state.Message, 
+                closing: true 
+            } 
+        });
+        setTimeout(() => {
+            this.setState({
+                Message: {
+                    isVisible: false,
+                    closing: false,
+                    text: "",
+                    color: "",
+                    timer: null
+                }
+            });
+        }, 100);
     }
 
     exit() {
         this.props.exit();
-        this.setState({ showHiddenLinks: false });
-        setTimeout( () => {
-            history.push('/aut');
-        }, 100 )
+        setTimeout(() => {
+            history.push("/aut");
+        }, 100);
         localStorage.removeItem("token");
     }
 
@@ -92,82 +110,57 @@ class App extends React.Component {
         return (
             <div className={styles["body-app"]}>
 
-                <NavBar exit={this.exit} />
+                <NavBar showMessage={this.showMessage} />
                 
                 <Router history={history}>
                     <Route path="/aut">
-                        <EntryBarAut />
+                        <EntryBarAut showMessage={this.showMessage} />
                         <LanguageSwitch />
                     </Route>
 
                     <Route path="/reg">
-                        <EntryBarReg />
+                        <EntryBarReg showMessage={this.showMessage} />
                         <LanguageSwitch />
                     </Route>
             
                     <Route path="/bookmarks">
-                        <MyLinks />
+                        <MyLinks showMessage={this.showMessage}
+                            exit={this.exit} 
+                        />
                     </Route>
                 </Router>
 
                 {this.props.UserMenu && <UserMenu 
                     updateUserProps={this.updateUserProps}
-                    showHiddenLinks={this.state.showHiddenLinks}
                     exit={this.exit}
                 />}
 
-                {this.props.modal.DeleteUser && <Modal 
-                    exit={this.exit}
-                    text={this.props.elementNames.Modals.DeleteUserText}
-                    primaryButton={this.props.elementNames.Modals.buttonRemove}
-                    action={"deleteUser"}
+                {this.state.Message.isVisible && <Message data={this.state.Message}
+                    closeMessage={this.closeMessage}
                 />}
-
-                {this.props.modal.ChangePassword && <Modal 
-                    updateUserProps={this.updateUserProps}
-                    header={this.props.elementNames.Modals.ChangePasswordHeader}
-                    firstInput={{ placeholder: this.props.elementNames.Modals.placeholderOldPassword }}
-                    secondInput={{ placeholder: this.props.elementNames.Modals.placeholderNewPassword }}
-                    primaryButton={this.props.elementNames.Modals.buttonSave}
-                    action={"changePassword"}
-                />}
-
-                {this.props.modal.ChangePINcode && <Modal 
-                    updateUserProps={this.updateUserProps}
-                    header={this.props.elementNames.Modals.ChangePINcodeHeader}
-                    firstInput={{ placeholder: this.props.elementNames.Modals.placeholderOldPINcode }}
-                    secondInput={{ placeholder: this.props.elementNames.Modals.placeholderNewPINcode }}
-                    primaryButton={this.props.elementNames.Modals.buttonSave}
-                    action={"changePINcode"}
-                />}
-
-                {this.props.message.isVisible && <Message />}
 
             </div>
-        )
+        );
     }
 }
 
-function mapStateToProps( state ) {
+function mapStateToProps(state) {
     return {
         UserMenu: state.UserMenu,
         modal: state.modal,
-        message: state.message,
-        user: state.user,
-        elementNames: state.elementNames
-    }
+        appData: state.appData
+    };
 }
 
-function mapDispatchToProps( dispatch ) {
+function mapDispatchToProps(dispatch) {
     return {
-        authorization: ownProps => dispatch( authorization( ownProps ) ),
-        setLanguage: ownProps => dispatch( setLanguage( ownProps ) ),
+        setAppData: (ownProps) => dispatch(setAppData(ownProps)),
         exit: () => {
             dispatch({
                 type: "EXIT"
-            })
+            });
         }
-    }
+    };
 }
 
-export default connect( mapStateToProps, mapDispatchToProps )( App );
+export default connect(mapStateToProps, mapDispatchToProps)(App);
