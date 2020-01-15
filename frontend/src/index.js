23cbefacc498a617c9625725ab65f6280e6a5be4
LoKi1237 <loki1237@yandex.ru>
2020-01-15 12:01:10 +0300

    Stylized code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducers } from './reducers/index';
@@ -11,6 +10,6 @@ const store = createStore(reducers);
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
-    document.getElementById( 'root' )
+    </Provider>,
+    document.getElementById('root')
 );
