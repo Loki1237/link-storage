c810e70eb1858d3eec705e5e55d0aec27cec5103
LoKi1237 <loki1237@yandex.ru>
2020-01-15 12:16:53 +0300

    Stylized code

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,24 +2,22 @@ import React from 'react';
 import styles from './NavBar.css';
 
 class NavBar extends React.Component {
-    
     render() {
         return (
             <nav className={styles.NavBar}>
 
-                <div className={styles["NavBar-label"]}>
+                <div className={styles.label}>
                     Links Storage
                 </div>
 
-                {this.props.user.id && <button className={styles["NavBar-item"]}
+                {this.props.appData.user.id && <button className={styles.item}
                     onClick={ this.props.openUserMenu }>
-                    {this.props.user.name}
+                    {this.props.appData.user.name}
                 </button>}
 
             </nav>
-        )
+        );
     }
-  
 }
   
 export default NavBar;
