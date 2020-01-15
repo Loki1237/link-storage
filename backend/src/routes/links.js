afc1d10e170aef5af8bb5ed262b99d22931d53fb
LoKi1237 <loki1237@yandex.ru>
2020-01-15 11:53:49 +0300

    Added bookmark checking

--- backend/src/routes/links.js
+++ backend/src/routes/links.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const typeorm = require("typeorm");
+const typeorm = require('typeorm');
 
-typeorm.createConnection().then( connection => {
+typeorm.createConnection().then(connection => {
     const linkRepository = connection.getRepository("Links");
+    const userRepository = connection.getRepository("Users");
 
     router.get('/:userID', async function(req, res) {
-        const links = await linkRepository.find({ userID: req.params.userID });
-        res.json(links);
+        const user = await userRepository.findOne({ id: req.params.userID });
+
+        const allLinks = await linkRepository.find({ userID: req.params.userID });
+        const visibleLinks = allLinks.filter(item => item.isVisible);
+
+        res.json(user.show === "all" ? allLinks : visibleLinks);
     });
 
     router.post('/', async function(req, res) {
         const link = await linkRepository.create(req.body);
         await linkRepository.save(link);
         return res.sendStatus(200);
-    })
+    });
 
     router.put('/:id', async function(req, res) {
         const link = await linkRepository.findOne({ id: req.params.id });
         await linkRepository.merge(link, req.body);
         await linkRepository.save(link);
         return res.sendStatus(200);
-    })
+    });
 
     router.delete('/:id', async function(req, res) {
         await linkRepository.delete({ id: req.params.id });
