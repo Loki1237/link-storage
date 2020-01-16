8320150ac9d2e83b0d8aaf0ea4f0987d9e9b3a63
LoKi1237 <loki1237@yandex.ru>
2020-01-17 01:49:52 +0300

    Added header

--- frontend/src/components/subcomponents/Message.js
+++ frontend/src/components/subcomponents/Message.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import styles from './Message.css';
+import info from '../images/message_info.png';
+import success from '../images/message_success.png';
+import warning from '../images/message_warning.png';
+import danger from '../images/message_danger.png';
+
+const icons = { info, success, warning, danger };
 
 function Message(props) {
     return (
         <div className={`${styles.Message} ${styles[`${props.data.color}`]}
             ${props.data.closing ? styles.close : ""}`}>
-            
-            <div className={`${styles.badge} ${styles[`badge-${props.data.color}`]}`}></div>
-            <span>{props.data.text}</span>
 
-            <button className={styles["close-button"]}
-                onClick={props.closeMessage}>
-            </button>
+            <div className={styles.header}>
+                <div className={styles.badge}>
+                    <img src={icons[`${props.data.color}`]} width="14" height="14" />
+                </div>
+
+                <span>{props.data.color}</span>
+
+                <button className={styles["close-button"]}
+                    onClick={props.closeMessage}>
+                </button>
+            </div>
+
+            <div className={styles.content}>
+                <span>{props.data.text}</span>
+            </div>
         </div>
     );
 }
