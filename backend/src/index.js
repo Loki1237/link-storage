ab65ff06381b1b5e7b047b9cc3634702e711ace0
LoKi1237 <loki1237@yandex.ru>
2020-06-17 08:12:43 +0300

    Added process env variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const typeorm = require("typeorm");
+const cookieParser = require('cookie-parser');
+const typeorm = require('typeorm');
+const EntitySchema = typeorm.EntitySchema;
 
-const home = require('./routes/home');
-const users = require('./routes/users');
-const links = require('./routes/links');
+const dotenv = require('dotenv');
 
-const app = express();
-const port = 3001;
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users');
+const linkRouter = require('./routes/links');
 
-typeorm.createConnection().then( () => {
+dotenv.config();
+
+typeorm.createConnection({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    entities: [
+        new EntitySchema(require("./entity/user.json")),
+        new EntitySchema(require("./entity/link.json"))
+    ]
+}).then(() => {
+    const app = express();
+    const port = 3001;
+
     app.use(bodyParser.json());
+    app.use(cookieParser());
 
-    app.use('/api', home);
-    app.use('/api/users', users);
-    app.use('/api/links', links);
+    app.use('/api/auth', authRouter());
+    app.use('/api/users', userRouter());
+    app.use('/api/links', linkRouter());
 
     app.listen(port, () => console.log(`Server is running on port ${port}`));
-} );
-
+}).catch(error => {
+    console.log("Error: ", error);
+});
