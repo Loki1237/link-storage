d7115dc8ce03eeeed98babbf1d9e69afe9e2bfe1
LoKi1237 <loki1237@yandex.ru>
2020-06-17 16:49:12 +0300

    Used material-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { reducers } from './reducers/index';
+import configureStore from './store/configureStore';
 
-const store = createStore(reducers);
+import { SnackbarProvider } from 'notistack';
+import amber from '@material-ui/core/colors/amber';
+import teal from '@material-ui/core/colors/teal';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const store = configureStore();
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: teal[600],
+        },
+        secondary: {
+            main: amber[600],
+        },
+    },
+});
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ThemeProvider theme={theme}>
+            <SnackbarProvider>
+                <App />
+            </SnackbarProvider>
+        </ThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
